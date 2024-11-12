10960b1559f890d05ca15387f83141f43bee132c
cristian <dussan29@gmail.com>
2024-11-11 23:08:45 -0500

    feature: add new comfigration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: "build", 
-
-    images: { unoptimized: true }
+    reactStrictMode: true,
+    // otras configuraciones
 };
 
 export default nextConfig;
