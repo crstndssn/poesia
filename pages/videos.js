dde7b44ea4d17ccf0289361e599f2971d0a207fe
cristian <dussan29@gmail.com>
2024-11-17 08:36:54 -0500

    feature: new letters

--- pages/videos.js
+++ pages/videos.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '@/app/globals.css'
 import Navigation from '@/components/Navigation'
+import CountdownTimer from '@/components/CountdownTimer'
 
 const videos = () => {
     return (
         <>
             <Navigation />
-            <div className="min-h-screen flex items-start justify-center bg-[#fffff9] bg-[url('/24.png')] bg-repeat-x bg-cover bg-center"> <Countimer /> </div>
+            <div className="min-h-screen flex items-start justify-center bg-[#fffff9] bg-[url('/24.png')] bg-repeat-x bg-cover bg-center"> <CountdownTimer /> </div>
         </>
     )
 }
