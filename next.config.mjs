383f284d6d0cea2126f848c849d66e18b4af8df4
cristian <dussan29@gmail.com>
2024-11-28 19:25:47 -0500

    feature: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Mantén reactStrictMode habilitado para verificar buenas prácticas
-
-    // Configuración adicional para evitar errores con bibliotecas como `pdfjs-dist`
-    webpack: (config) => {
-        config.resolve.fallback = {
-            fs: false, // Evita errores relacionados con el módulo 'fs'
-        };
-        return config;
-    },
+    reactStrictMode: true,// Mantén reactStrictMode habilitado para verificar buenas prácticas
 };
 
 export default nextConfig;
