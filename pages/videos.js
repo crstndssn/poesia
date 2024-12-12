3a604ae25eff6db190463fa5afb90666f82d0b7d
cristian <dussan29@gmail.com>
2024-12-11 19:52:47 -0500

    feature: update video section

--- pages/videos.js
+++ pages/videos.js
@@ -11,43 +11,36 @@ const videos = () => {
                 <div className="container mx-auto flex flex-col mt-40">
                     <h2 className="text-3xl font-bold">Martes de Poesia</h2>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        <iframe
-                            src="https://www.youtube.com/embed/URL_DEL_VIDEO_1"
-                            className="w-full rounded shadow-lg"
-                            title="Martes de Poesía en la sede La Esperanza"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                        <iframe
-                            src="https://www.youtube.com/embed/URL_DEL_VIDEO_2"
-                            className="w-full rounded shadow-lg"
-                            title="Video de Martes de Poesía primera sistematización de la experiencia"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                        <iframe
-                            src="https://www.youtube.com/embed/URL_DEL_VIDEO_3"
-                            className="w-full rounded shadow-lg"
-                            title="Martes De Poesía 3"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/V5rKClYRY6w?si=1dFNDazdQ5dcGhM-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
-                    <h2 className="text-3xl font-bold">Encuentro Zonal de Experiencias Pedagógicas Significativas</h2>
+                    <h2 className="text-3xl font-bold">Sistematización Martes de Poesía. Clip 1.</h2>
+                    <section>
+                        <iframe width="315" height="560"
+                            src="https://www.youtube.com/embed/wn1iA7ZujAM"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+
+                    </section>
+
+                    <h2 className="text-3xl font-bold">Sistematización Martes de Poesía. Clip 2.</h2>
                     <section>
                         <iframe
-                            src="https://www.youtube.com/embed/URL_DEL_VIDEO_4"
+                            width="315" height="560"
+                            src="https://www.youtube.com/embed/2Hre_W9ZKrM"
                             className="w-full rounded shadow-lg"
-                            title="Encuentro Zonal"
+                            title="Foro Educativo 2014"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                         ></iframe>
                     </section>
 
-                    <h2 className="text-3xl font-bold">Foro Educativo 2014</h2>
+                    <h2 className="text-3xl font-bold">Sistematización Martes de Poesía. Clip 3.</h2>
                     <section>
                         <iframe
-                            src="https://www.youtube.com/embed/URL_DEL_VIDEO_5"
+                            width="315" height="560"
+                            src="https://www.youtube.com/embed/GdZZieUIQKk"
                             className="w-full rounded shadow-lg"
                             title="Foro Educativo 2014"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
